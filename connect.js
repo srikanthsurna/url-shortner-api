f1b62f00ce89a406fb312935403b955a84f37b74
surna srikanth <122173096+srikanthsurna@users.noreply.github.com>
2024-06-02 16:30:14 +0530

    Add files via upload

---
+++ connect.js
@@ -0,0 +1,5 @@
+const mongoose=require("mongoose");
+async function connectTOMongoDB(url){
+    return mongoose.connect(url);
+}
+module.exports={connectTOMongoDB,};
---
+++ index.js
@@ -0,0 +1,27 @@
+const express =require("express");
+const {connectTOMongoDB}=require("./connect")
+const urlRoute=require('./routes/url');
+const URL=require('./models/url');
+const app=express();
+const PORT=process.env.PORT || 3000;
+require('dotenv').config();
+const databaseUrl = process.env.CONNECTIONSTRING;
+connectTOMongoDB(databaseUrl).then(()=> console.log('Mongodb connected'));
+app.use(express.json());
+app.use('/url',urlRoute);
+app.get('/:shortId',async (req,res)=>{
+    const shortId=req.params.shortId;
+   const entry = await URL.findOneAndUpdate(
+        { shortId,},
+        {
+            $push:{
+                visitHistory: {
+                    timestamp :Date.now(),
+                }
+            }
+        }
+    );
+    res.redirect(entry.redirectURL);
+});
+
+app.listen(PORT,()=> console.log(`Server started at port ${PORT}`));
