4a8695d5b19250ebe47031ea30721b987c1d6102
surna srikanth <122173096+srikanthsurna@users.noreply.github.com>
2024-06-02 17:18:17 +0530

    Update url.js
    short id of length 5

--- controllers/url.js
+++ controllers/url.js
@@ -3,7 +3,8 @@ const shortid=require("shortid");
 async function handleGenerateNewShortURL(req,res){
     const body=req.body;
     if(!body.url) return res.status(400).json({error:'url is required'});
-    const shortID=shortid();
+    // const shortID=shortid();
+    const shortID=shortid.generate().substring(0,5);
     await URL.create({
         shortId : shortID,
         redirectURL: body.url,
