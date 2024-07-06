bc6ad01aea5cf9250b2b52c3a8eb715977e34e08
surna srikanth <122173096+srikanthsurna@users.noreply.github.com>
2024-07-06 19:48:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express =require("express");
+const cors =require('cors');
 const {connectTOMongoDB}=require("./connect")
 const urlRoute=require('./routes/url');
 const URL=require('./models/url');
 const app=express();
+app.use(cors());
 const PORT=process.env.PORT || 3000;
 require('dotenv').config();
 const databaseUrl = process.env.CONNECTIONSTRING;
