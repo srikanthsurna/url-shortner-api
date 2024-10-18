c8a51a0afce3ea3f048f51f5f4651263baad442d
Surna srikanth <122173096+srikanthsurna@users.noreply.github.com>
2024-10-18 21:19:33 +0530

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
 async function connectTOMongoDB(url){
-    return mongoose.connect(url);
+    return mongoose.connect("mongodb+srv://srikanth:srikanth@cluster0.xqig8o8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 }
 module.exports={connectTOMongoDB,};
