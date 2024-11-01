9fd67839196aa5505ef331f3b88cb8cfbb31853c
Surna srikanth <122173096+srikanthsurna@users.noreply.github.com>
2024-11-01 19:27:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ require('dotenv').config();
 const databaseUrl = process.env.CONNECTIONSTRING;
 connectTOMongoDB(databaseUrl).then(()=> console.log('Mongodb connected'));
 app.use(express.json());
-app.all('*', function(req, res) {
-  res.send("This is the Home page please use an actual API  --- Made by srikanth Surna");
-});
+
 app.use('/url',urlRoute);
 app.get('/:shortId',async (req,res)=>{
     const shortId=req.params.shortId;
